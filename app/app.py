f4e46dd652235ffb0a6769058a808fbf0f88c0f5
Lara/Jimbo <121173492+coolspaghetti@users.noreply.github.com>
2024-12-02 14:59:17 +0100

    added login page

--- app/app.py
+++ app/app.py
@@ -8,6 +8,7 @@ from .constants.routes import Route
 from .pages.example import example_page
 from .pages.index import index_page
 from .styles.base import BASE_STYLE
+from .pages.login import login_page
 
 
 # https://reflex.dev/docs/styling/theming/
@@ -20,6 +21,7 @@ THEME = rx.theme(
 app = rx.App(style=BASE_STYLE, theme=THEME)
 app.add_page(index_page(), route=Route.ROOT)
 app.add_page(example_page(), route=Route.EXAMPLE)
+app.add_page(login_page(), route=Route.LOGIN)
 
 # Loads the development env file as fallback if no env file was loaded
 # e.g. if the app was started outside the docker container
